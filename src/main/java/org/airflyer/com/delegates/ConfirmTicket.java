ffc42239db40ed72ba6376ee21a75eb935c22860
bernardoco <bernardoco@poli.ufrj.br>
2021-06-03 15:57:07 -0300

    Update service task

--- src/main/java/org/airflyer/com/delegates/ConfirmTicket.java
+++ src/main/java/org/airflyer/com/delegates/ConfirmTicket.java
@@ -1,9 +1,12 @@
-package org.airflyer.com.travel.delegates;
+package org.airflyer.com.delegates;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import org.airflyer.com.service.dto.BuyPlaneTicketDTO;
+import org.airflyer.com.domain.BuyTicket;
 
 import java.math.BigDecimal;
 
@@ -12,10 +15,17 @@ public class ConfirmTicket implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-         System.out.println("==================================================");
-         System.out.println("=============== RESERVA CONFIRMADA ===============");
-         System.out.println("==================================================");
+        BuyPlaneTicketDTO pi = (BuyPlaneTicketDTO) delegateExecution.getVariable("pi");
 
+        String name = pi.getBuyTicket().getName();
+
+        System.out.println();
+        System.out.println("=============================================");
+        System.out.println("RESERVA CONFIRMADA");
+        System.out.println(name);
+        System.out.println("OBRIGADO POR ESCOLHER A AIR FLYER");
+        System.out.println("=============================================");
+        System.out.println();
     }
 
 }
